65fd9adecc78dfb4fb1dcdfbbbcc49dd16aaecdb
Marek Reszke <m.reszke@livechat.com>
2026-02-06 15:36:41 +0100

    Update packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import pkg from './package.json';
+import pkg from './package.json'
 import resolve from '@rollup/plugin-node-resolve';
 import {babel} from '@rollup/plugin-babel';
-import {terser} from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import serve from 'rollup-plugin-serve';
 
